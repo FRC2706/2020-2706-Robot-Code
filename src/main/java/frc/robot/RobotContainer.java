4aac0c780c900aaed9f0d60f43516081ce440460
Ethan Tovell <erwtovell@outlook.com>
2020-01-11 15:00:34 -0500

    Made DriveUntilColor Command
    Made the command file for DriveUntilColor command.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,7 +9,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
+import frc.robot.commands.DriveUntilColor;
 import frc.robot.subsystems.ColorSensorSubsystem;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -26,7 +26,7 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final ColorSensorSubsystem m_colorSubsystem = new ColorSensorSubsystem();
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+  private final DriveUntilColor m_autoCommand = new DriveUntilColor(m_exampleSubsystem);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
