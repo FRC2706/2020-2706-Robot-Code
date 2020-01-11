ca462946cedaf83ca7210dfb9c6c65d5f5b669fe
Justin Toft <justintoft10@gmail.com>
2020-01-11 13:30:09 -0500

    Optimized imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,13 +7,9 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.config.XboxValue;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
