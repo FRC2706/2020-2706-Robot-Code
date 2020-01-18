f34ca95ce407cd02169c928ca1e40f020978660e
Justin Toft <justintoft10@gmail.com>
2020-01-18 09:20:30 -0500

    Removed code that will cause a compilation error

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -52,7 +52,6 @@ public class Robot extends TimedRobot {
     // block in order for anything in the Command-based framework to work.
 
     CommandScheduler.getInstance().run();
-    System.out.println(Config.RIGHT_FRONT_DRIVEBASE_TALON);
   }
 
   /**
