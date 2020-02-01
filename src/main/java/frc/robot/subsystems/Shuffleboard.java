b36dcb7977c38eda488238346a09b7362dfc5548
Jacob T <jwt2home@gmail.com>
2020-02-01 14:17:08 -0500

    Made the variable names more clear #9

--- src/main/java/frc/robot/subsystems/Shuffleboard.java
+++ src/main/java/frc/robot/subsystems/Shuffleboard.java
@@ -18,7 +18,7 @@ import edu.wpi.first.wpilibj.Timer;
 public class Shuffleboard extends SubsystemBase {
   private final Joystick joystick = new Joystick(0);
   private final Timer m_timer = new Timer();
-  private int SecondsLeft = 150;
+  private int SecondsLeftInMatch = 150; // 150 (seconds) = 2:30 minutes
 
   private static Shuffleboard INSTANCE = new Shuffleboard();
 
@@ -28,7 +28,7 @@ public class Shuffleboard extends SubsystemBase {
   private Shuffleboard() {
     CameraServer.getInstance().startAutomaticCapture();
     m_timer.start();
-    SmartDashboard.putNumber("Time Left", SecondsLeft);
+    SmartDashboard.putNumber("Time Left", SecondsLeftInMatch);
   }
 
   public static Shuffleboard getINSTANCE() {
@@ -37,54 +37,53 @@ public class Shuffleboard extends SubsystemBase {
 
   @Override
   public void periodic() {
-
     // This method will be called once per scheduler run
     // shift alt F
-    if (SecondsLeft > 0) {
+    if (SecondsLeftInMatch > 0) {
       SmartDashboard.putNumber("Timer", m_timer.get());
       if (m_timer.get() > 1) {
         m_timer.stop();
         m_timer.reset();
-        SmartDashboard.putNumber("Time Left", SecondsLeft);
-        SecondsLeft = SecondsLeft - 1;
+        SmartDashboard.putNumber("Time Left", SecondsLeftInMatch);
+        SecondsLeftInMatch = SecondsLeftInMatch - 1;
         m_timer.start();
       }
-      SmartDashboard.putBoolean("ENDGAME", SecondsLeft < 30);
-      SmartDashboard.putBoolean("GAME OVER", SecondsLeft < 1);
+      SmartDashboard.putBoolean("ENDGAME", SecondsLeftInMatch < 30);
+      SmartDashboard.putBoolean("GAME OVER", SecondsLeftInMatch < 1);
 
       boolean buttonValueA; // A is 1
       boolean buttonValueB; // B is 2
       boolean buttonValueX; // X is 3
       boolean buttonValueY; // Y is 4
 
-      double XvalueL = joystick.getRawAxis(0);
-      double YvalueL = joystick.getRawAxis(1);
-      double XvalueR = joystick.getRawAxis(4);
-      double YvalueR = joystick.getRawAxis(5);
+      double LeftXvalue = joystick.getRawAxis(0);
+      double LeftYvalue = joystick.getRawAxis(1);
+      double RightXvalue = joystick.getRawAxis(4);
+      double RightYvalue = joystick.getRawAxis(5);
 
       // Left Joystick
-      SmartDashboard.putNumber("Joystick XL Value", XvalueL);
-      SmartDashboard.putNumber("Joystick YL Value", YvalueL * -1);
-      SmartDashboard.putBoolean("XL Too Slow?", XvalueL < -0.9);
-      SmartDashboard.putBoolean("XL Too Fast?", XvalueL > 0.9);
-      SmartDashboard.putBoolean("YL Too Fast?", YvalueL < -0.9);
-      SmartDashboard.putBoolean("YL Too Slow?", YvalueL > 0.9);
+      SmartDashboard.putNumber("Left Joystick X Value", LeftXvalue);
+      SmartDashboard.putNumber("Left Joystick Y Value", LeftYvalue * -1);
+      SmartDashboard.putBoolean("Left X Too Slow?", LeftXvalue < -0.9);
+      SmartDashboard.putBoolean("Left X Too Fast?", LeftXvalue > 0.9);
+      SmartDashboard.putBoolean("Left Y Too Fast?", LeftYvalue < -0.9);
+      SmartDashboard.putBoolean("Left Y Too Slow?", LeftYvalue > 0.9);
 
       // Right Joystick
-      SmartDashboard.putNumber("Joystick XR Value", XvalueR);
-      SmartDashboard.putNumber("Joystick YR Value", YvalueR * -1);
-      SmartDashboard.putBoolean("XR Too Slow?", XvalueR < -0.9);
-      SmartDashboard.putBoolean("XR Too Fast?", XvalueR > 0.9);
-      SmartDashboard.putBoolean("YR Too Fast?", YvalueR < -0.9);
-      SmartDashboard.putBoolean("YR Too Slow?", YvalueR > 0.9);
+      SmartDashboard.putNumber("Right Joystick X Value", RightXvalue);
+      SmartDashboard.putNumber("Right Joystick Y Value", RightYvalue * -1);
+      SmartDashboard.putBoolean("Right X Too Slow?", RightXvalue < -0.9);
+      SmartDashboard.putBoolean("Right X Too Fast?", RightXvalue > 0.9);
+      SmartDashboard.putBoolean("Right Y Too Fast?", RightYvalue < -0.9);
+      SmartDashboard.putBoolean("Right Y Too Slow?", RightYvalue > 0.9);
 
-      if (XvalueL < -0.9 || XvalueL > 0.9 || YvalueL < -0.9 || YvalueL > 0.9 || XvalueR < -0.9 || XvalueR > 0.9
-          || YvalueR < -0.9 || YvalueR > 0.9) {
+      if (LeftXvalue < -0.9 || LeftXvalue > 0.9 || LeftYvalue < -0.9 || LeftYvalue > 0.9 || RightXvalue < -0.9 || RightXvalue > 0.9
+          || RightYvalue < -0.9 || RightYvalue > 0.9) {
         joystick.setRumble(RumbleType.kLeftRumble, 1);
         joystick.setRumble(RumbleType.kRightRumble, 1);
       } else {
-        if (XvalueL < -0.8 || XvalueL > 0.8 || YvalueL < -0.8 || YvalueL > 0.8 || XvalueR < -0.8 || XvalueR > 0.8
-            || YvalueR < -0.8 || YvalueR > 0.8) {
+        if (LeftXvalue < -0.8 || LeftXvalue > 0.8 || LeftYvalue < -0.8 || LeftYvalue > 0.8 || RightXvalue < -0.8 || RightXvalue > 0.8
+            || RightYvalue < -0.8 || RightYvalue > 0.8) {
           joystick.setRumble(RumbleType.kLeftRumble, 0.5);
           joystick.setRumble(RumbleType.kRightRumble, 0.5);
         } else {
