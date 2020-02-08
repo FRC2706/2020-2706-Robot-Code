be26143cb4f905561ea65368de5b27e8bf1b71ea
Jacob T <jwt2home@gmail.com>
2020-02-08 15:41:47 -0500

    fixed robot.java in my branch

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,16 +9,12 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj.GenericHID.RumbleType;
-import edu.wpi.first.cameraserver.CameraServer;
-//import edu.wpi.first.wpilibj.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import frc.robot.config.Config;
 import frc.robot.subsystems.DriveBase;
-import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.nettables.*;
 import edu.wpi.first.wpilibj.DriverStation;
-import frc.robot.subsystems.Shuffleboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -27,15 +23,9 @@ import frc.robot.subsystems.Shuffleboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  private final Joystick joystick = new Joystick(0);
-  //private double kP = 0.5;  // This was for a pid that is currently not finished...
-  //private double kI = 0.5;
-  //private double kD = 0.5;
-  //private int setpoint = 0;
-  
   private Command m_autonomousCommand;
 
-    private RobotContainer m_robotContainer;
+  private RobotContainer m_robotContainer;
 
   //network table for vision control
   private VisionCtrlNetTable visionControlNetTable;
@@ -47,16 +37,14 @@ public class Robot extends TimedRobot {
   private Boolean bRealMatch;
 
   /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
+   * This function is run when the robot is first started up and should be used for any
+   * initialization code.
    */
   @Override
   public void robotInit() {
-  CameraServer.getInstance().startAutomaticCapture();
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     DriveBase.init();
-    FeederSubsystem.init();
 
     m_robotContainer = new RobotContainer();
     
@@ -69,18 +57,27 @@ public class Robot extends TimedRobot {
     bRealMatch = false;
   }
 
-    private RobotContainer m_robotContainer;
+  /**
+   * This function is called every robot packet, no matter the mode. Use this for items like
+   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() {
+    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
+    // commands, running already-scheduled commands, removing finished or interrupted commands,
+    // and running subsystem periodic() methods.  This must be called from the robot's periodic
+    // block in order for anything in the Command-based framework to work.
 
-    /**
-     * This function is run when the robot is first started up and should be used for any
-     * initialization code.
-     */
-    @Override
-    public void robotInit() {
-        // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-        // autonomous chooser on the dashboard.
-        DriveBase.init();
+    CommandScheduler.getInstance().run();
+  }
 
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   */
+  @Override
   public void disabledInit() {
     if (bRealMatch == true && bFromTeleMode == true) 
     {
@@ -94,36 +91,21 @@ public class Robot extends TimedRobot {
 
   }
 
+  @Override
+  public void disabledPeriodic() {
+  }
+
+  /**
+   * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
+   */
+  @Override
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    // schedule the autonomous command (example)
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
     }
-
-    /**
-     * This function is called every robot packet, no matter the mode. Use this for items like
-     * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-     *
-     * <p>This runs after the mode specific periodic functions, but before
-     * LiveWindow and SmartDashboard integrated updating.
-     */
-    @Override
-    public void robotPeriodic() {
-        // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-        // commands, running already-scheduled commands, removing finished or interrupted commands,
-        // and running subsystem periodic() methods.  This must be called from the robot's periodic
-        // block in order for anything in the Command-based framework to work.
-
-        CommandScheduler.getInstance().run();
-        Shuffleboard.getINSTANCE().periodic();
-    }
-
-    /**
-     * This function is called once each time the robot enters Disabled mode.
-     */
-    @Override
-    public void disabledInit() {
-
-    }
-    else {
-    joystick.setRumble(RumbleType.kLeftRumble, 0);
-    joystick.setRumble(RumbleType.kRightRumble, 0);
 
     //send start up signal to vision team
     visionControlNetTable.startUpVision();
@@ -132,39 +114,34 @@ public class Robot extends TimedRobot {
     bRealMatch = isRealMatch();
   }
 
-    @Override
-    public void disabledPeriodic() {
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() {
+  }
 
-    }
-
-    /**
-     * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
-     */
-    @Override
-    public void autonomousInit() {
-        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-        // schedule the autonomous command (example)
-        if (m_autonomousCommand != null) {
-            m_autonomousCommand.schedule();
-        }
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when
+    // teleop starts running. If you want the autonomous to
+    // continue until interrupted by another command, remove
+    // this line or comment it out.
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
     }
     
     //set teleop mode to true
     bFromTeleMode = true;
 
-    else {
-    joystick.setRumble(RumbleType.kLeftRumble, 0);
-    joystick.setRumble(RumbleType.kRightRumble, 0);
   }
 
-    /**
-     * This function is called periodically during autonomous.
-     */
-    @Override
-    public void autonomousPeriodic() {
-
-    }
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() {
+  }
 
   @Override
   public void testInit() {
@@ -173,20 +150,7 @@ public class Robot extends TimedRobot {
 
     //set teleop mode to false
     bFromTeleMode = false;
-  buttonValueA = joystick.getRawButton(1); // Button A
-  if(buttonValueA) {SmartDashboard.putBoolean("A", buttonValueA);
   }
-  buttonValueB = joystick.getRawButton(2); // Button B
-  if (buttonValueB) {SmartDashboard.putBoolean("B", buttonValueB);
-  }
-  buttonValueX = joystick.getRawButton(3); // Button X
-  if (buttonValueX) {SmartDashboard.putBoolean("X", buttonValueX);
-  } 
-  buttonValueY = joystick.getRawButton(4); // Button Y
-  if (buttonValueY) {SmartDashboard.putBoolean("Y", buttonValueY);
-  }
-  
-}
 
   /**
    * This function is called periodically during test mode.
@@ -203,20 +167,4 @@ public class Robot extends TimedRobot {
   public static boolean isRealMatch() {
     return DriverStation.getInstance().isFMSAttached();
   }
-}
-
-    }
-
-    @Override
-    public void testInit() {
-        // Cancels all running commands at the start of test mode.
-        CommandScheduler.getInstance().cancelAll();
-    }
-
-    /**
-     * This function is called periodically during test mode.
-     */
-    @Override
-    public void testPeriodic() {
-    }
 }
