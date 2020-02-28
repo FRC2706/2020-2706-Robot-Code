53720fb9c221d867bd8b575b5a3c0b45ca73f8bc
davidliu-2706 <dliu5@ocdsb.ca>
2020-02-27 22:53:13 -0500

    issue #59 adjusted formula and variable names

--- src/main/java/frc/robot/commands/VisionAssistedShooter.java
+++ src/main/java/frc/robot/commands/VisionAssistedShooter.java
@@ -16,52 +16,56 @@ import frc.robot.nettables.VisionCtrlNetTable;
 public class VisionAssistedShooter extends CommandBase {
   
   //subsystem
-  private final ShooterSubsystem m_shooter;
+  private final ShooterSubsystem shooter;
     
   //todo: can be configured in config file as well
-  private final double m_theta  = 60.0;    //unit: degree
-  private final double m_height = 2.49;    //unit: meter
+  //todo: measure the radius for the shooting wheel
+  private final double theta  = 60.0;    //unit: degree
+  private final double height = 2.49;    //unit: meter 
+  private final double radius = 10;//unit cm
 
   //values from Vision Network Table
-  private double m_distanceToOuterPort;
+  private double distanceToOuterPort;
    
   //network table for vision control
   private VisionCtrlNetTable visionControlNetTable;
 
   //calculated for the shooter
-  double m_targetRPM = 0;
-  double m_targetDistance = 0;
+  double targetDistance = 0;
+  double targetRPM = 0;
+
 
   /**
    * Creates a new ExampleCommand.
    *
    * @param subsystem The subsystem used by this command.
    */
-  public VisionAssistedShooter(ShooterSubsystem subsystem, DriveBase driveBase) {
+  public VisionAssistedShooter(ShooterSubsystem subsystem) {
    
     //subsystem
-    m_shooter = subsystem;
+    shooter = subsystem;
    
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(subsystem);
   }
 
-
   @Override
   public void initialize() {
+
     // Ensure the vision is running in tape mode
     visionControlNetTable.setTapeMode();
 
     //Read the network table from vision to get the distance from the power port.
     //distance from vision
-    m_distanceToOuterPort = visionControlNetTable.distanceToOuterPort.get();
+    //NOTE: unit should be meter. If not, need conversion here.
+    distanceToOuterPort = visionControlNetTable.distanceToOuterPort.get();
 
     //todo: to adjuste the distance for the shooter 
-    //m_targetDistance
+    //targetDistance
 
     //Calculate the RPM of the shooter wheel.
-    double targetV  = initVelocity( m_distanceToOuterPort, m_height, m_theta);
-    m_targetRPM     = velocityToRPM (targetV);
+    double targetV  = initVelocity( distanceToOuterPort, height, theta);
+    targetRPM     = velocityToRPM (targetV);
     
   }
 
@@ -69,7 +73,7 @@ public class VisionAssistedShooter extends CommandBase {
   public void execute() {
 
     //Set the shooter to the target RPM.
-    m_shooter.setTargetRPM((int) m_targetRPM);
+    shooter.setTargetRPM((int) targetRPM);
 
     //todo: provide feedback to the shuffleboard for Driver Team
     
@@ -86,9 +90,15 @@ public class VisionAssistedShooter extends CommandBase {
         
     }
 
+    //d: unit meter
+    //h: unit meter
+    //theta: unit degree
+    //return: unit m/s
   double initVelocity(double d, double h, double theta) {
     double dCheck = Math.tan(theta)*d - h;
     double dTemp;
+
+    //unit: m/s
     double dInitVelocity;
     if (dCheck > 0)
     {
@@ -105,14 +115,12 @@ public class VisionAssistedShooter extends CommandBase {
   
  }
 
- //todo: convert velocity t0 RPM
+ // convert velocity t0 RPM
+ // velocity: unit m/s
+ // return: unit revolutions per minute
 double velocityToRPM( double velocity)
- {
-     //todo: set to the constant table
-     double radius = 10;//cm
-     //then velocity's unit should be cm/minute
-
-     double rpm = velocity/(2*Math.PI*radius);
+ {     
+     double rpm = velocity*3000/(Math.PI*radius);
      return rpm;
  }
 
