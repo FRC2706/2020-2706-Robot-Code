7a1155798059f89ea06095c4be3779e4f6bfd36a
Lara <laralim10@gmail.com>
2020-02-29 09:59:53 -0500

    Fixed indentation

--- src/main/java/frc/robot/commands/SpinUpShooter.java
+++ src/main/java/frc/robot/commands/SpinUpShooter.java
@@ -4,46 +4,46 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShooterSubsystem;
 
 public class SpinUpShooter extends CommandBase {
-  
-  private ShooterSubsystem shooterSubsystem;
 
-  boolean doneRamping;
-  int RPM = 2000;
+    private ShooterSubsystem shooterSubsystem;
 
-  public SpinUpShooter() {
-    shooterSubsystem = ShooterSubsystem.getInstance();
-    if(shooterSubsystem.isActive()){
-      addRequirements(shooterSubsystem);
+    boolean doneRamping;
+    int RPM = 2000;
+
+    public SpinUpShooter() {
+        shooterSubsystem = ShooterSubsystem.getInstance();
+        if (shooterSubsystem.isActive()) {
+            addRequirements(shooterSubsystem);
+        }
     }
-  }
 
-  @Override
-  public void initialize() {
-  }
-
-  @Override
-  public void execute() {
-    shooterSubsystem.setTargetRPM(RPM);
-    doneRamping = shooterSubsystem.isAtTargetRPM();
-    if(doneRamping){
-      // Print to console
-      System.out.println("calculatedRPM is within 30 units of targetRPM");
+    @Override
+    public void initialize() {
     }
-  }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    shooterSubsystem.setTargetRPM(0);
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    if (!shooterSubsystem.isActive()){
-      return true;
-    } else {
-      return false;
+    @Override
+    public void execute() {
+        shooterSubsystem.setTargetRPM(RPM);
+        doneRamping = shooterSubsystem.isAtTargetRPM();
+        if (doneRamping) {
+            // Print to console
+            System.out.println("calculatedRPM is within 30 units of targetRPM");
+        }
     }
-  }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        shooterSubsystem.setTargetRPM(0);
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        if (!shooterSubsystem.isActive()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
