3d5bbf477b3377a5dc347dab5e4370c067c692ba
Lara <laralim10@gmail.com>
2020-03-10 20:36:36 -0400

    The climber exists

---
+++ src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -0,0 +1,55 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.config.Config;
+
+public class ClimberSubsystem extends SubsystemBase {
+
+    private WPI_TalonSRX climberTalon;
+
+    /**
+     * Creates a new ClimberSubsystem.
+     */
+    public ClimberSubsystem() {
+        if(Config.CLIMBER_TALON != -1) initializeClimber();
+    }
+
+    private void initializeClimber(){
+        climberTalon = new WPI_TalonSRX(Config.CLIMBER_TALON);
+
+        climberTalon.configPeakOutputForward(1);
+        climberTalon.configPeakOutputReverse(0);
+    }
+
+    private static class ClimberHolder {
+        private static final ClimberSubsystem INSTANCE_CLIMBER = new ClimberSubsystem();
+    }
+
+    public static ClimberSubsystem getInstance() {
+        return ClimberHolder.INSTANCE_CLIMBER;
+    }
+
+    // Add setting output to periodic?
+    public void runClimber(){
+        // Change this output probably
+        climberTalon.set(0.5);
+    }
+
+    public void stopClimber(){
+        climberTalon.set(0);
+    }
+
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+    }
+}
